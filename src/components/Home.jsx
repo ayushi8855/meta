f33477c7aee5440ddfe20ab013a1c9cb89bee322
ayushi <ayushi8855@gmail.com>
2023-01-07 16:04:42 +0530

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,18 +5,18 @@ export const Home = () => {
     const data = require("./class.json")
     console.log(data)
   return (
-    <div>
+    <div className='bg-light'>
         <div style={{ height:"300px" ,color:"white",paddingTop:"50px",marginBottom:"50px"}} className="bg-primary">
             <h2 >Title</h2>
             <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h4>
             <h4>Lorem Ipsum is simply dummy </h4>
             <Button variant="light">Read More</Button>
         </div>
-        <div>
+        <div >
         <p className="fs-2">Classes</p>
         <p style={{color:"grey",fontSize:"20px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy</p>
         </div>
-        <div className='d-flex justify-content-around flex-wrap w-75 m-auto'>
+        <div className='d-flex justify-content-around flex-wrap w-75 m-auto '>
         { data.map((e)=>(
         
         <div style={{width:"300px" ,padding:"40px",margin:"20px",textAlign:"left"}} className="shadow p-3">
@@ -30,7 +30,7 @@ export const Home = () => {
         </div>
     ))}
         </div>
-      <div className='d-flex justify-content-between p-4'>
+      <div className='d-flex justify-content-between p-4 bg-white'>
         <p style={{color:"grey"}}>Copyright @ 2021. All Right Reserved</p>
         <p className='text-primary'>Back To Top</p>
       </div>
